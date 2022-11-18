92f76d6da052c91b285b7062652aba2dd1390855
Perseus <54466064+Perseusx99007@users.noreply.github.com>
2022-11-19 00:57:15 +0530

    Database-Creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,76 @@
+import React from "react";
 import "./styles.css";
+import { useState } from "react";
+
+const bookDataBase = {
+  coding: [
+    { name: "Eloquent JavaScript", author: "Marijn Haverbeke" },
+    { name: "Seven Languages in Seven Weeks", author: "Bruce Tate" },
+    { name: "Fundamentals of Computers", author: "E. Balagurusamy" }
+  ],
+
+  fiction: [
+    { name: "The Percy Jckson Series", author: "Rick Rirdan" },
+    { name: "The Shiva Trilogy", author: "Amish Tripathi" },
+    { name: "The Harry Potter Series", author: "J.K. Rowling" }
+  ],
+
+  business: [
+    {
+      name: "Never Split the Difference",
+      author: "Christopher Voss and Tahl Raz"
+    },
+    { name: "Loonshots", author: "Safi Bahcall" },
+    { name: "The Hard Thing About Hard Things", author: "Ben Horowitz" }
+  ]
+};
+
+const songDataBase = {
+  rock: [
+    { name: "One  Moe Light", singer: "Linkin Park" },
+    { name: "Stars", singer: "Skillet" },
+    { name: "I Will Not Bow", singer: "Breaking Benjamin" }
+  ],
+
+  soul: [
+    { name: "Visiting Hours", singer: "Ed Sheeran" },
+    { name: "Dil Ka Dariya", singer: "Arijit Sing" },
+    { name: "Teri Mitti", singer: "B Praak" }
+  ],
+
+  rap: [
+    { name: "Lose Yourself", singer: "EMINEM" },
+    { name: "Aathma Raama", singer: "Brodha V" },
+    { name: "Lost in The Echo", singer: "Linkin Park" }
+  ]
+};
+
+const movieDataBase = {
+  action: [
+    { name: "Marvel Avengers Series", director: "Anthony & Joe Russo" },
+    { name: "Justice League", director: "Zack Snyder" },
+    { name: "R.R.R.", director: "S. S. Rajamouli" }
+  ],
+
+  thriller: [
+    { name: "Rocketry: The Nambi Effect", director: "R Madhavan" },
+    { name: "Uri: The Surgical Strike", director: "Aditya Dhar" },
+    { name: "Shershaah", director: "Vishnuvardhan" }
+  ],
+
+  sliceOfLife: [
+    { name: "777 Charlie", director: "Kiranraj K" },
+    { name: "Hachi: A Dog's Tale", director: "Lasse Hallström" },
+    { name: "Dead Poets Society", director: "Peter Weir" }
+  ]
+};
+
+var a = [songDataBase, bookDataBase, movieDataBase];
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>💯 ⭐Awesone Recommendations📚</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
