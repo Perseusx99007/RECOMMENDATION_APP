ef996be5285aba6dbfa67408e623a126bc80499b
Perseus <54466064+Perseusx99007@users.noreply.github.com>
2022-11-20 02:09:03 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -4,64 +4,64 @@ import { useState } from "react";
 
 const Books = {
   coding: [
-    { name: "Eloquent JavaScript", author: "Marijn Haverbeke" },
-    { name: "Seven Languages in Seven Weeks", author: "Bruce Tate" },
-    { name: "Fundamentals of Computers", author: "E. Balagurusamy" }
+    { name: "Eloquent JavaScript", contributor: "Marijn Haverbeke" },
+    { name: "Seven Languages in Seven Weeks", contributor: "Bruce Tate" },
+    { name: "Fundamentals of Computers", contributor: "E. Balagurusamy" }
   ],
 
   fiction: [
-    { name: "The Percy Jckson Series", author: "Rick Rirdan" },
-    { name: "The Shiva Trilogy", author: "Amish Tripathi" },
-    { name: "The Harry Potter Series", author: "J.K. Rowling" }
+    { name: "The Percy Jckson Series", contributor: "Rick Rirdan" },
+    { name: "The Shiva Trilogy", contributor: "Amish Tripathi" },
+    { name: "The Harry Potter Series", contributor: "J.K. Rowling" }
   ],
 
   business: [
     {
       name: "Never Split the Difference",
-      author: "Christopher Voss and Tahl Raz"
+      contributor: "Christopher Voss and Tahl Raz"
     },
-    { name: "Loonshots", author: "Safi Bahcall" },
-    { name: "The Hard Thing About Hard Things", author: "Ben Horowitz" }
+    { name: "Loonshots", contributor: "Safi Bahcall" },
+    { name: "The Hard Thing About Hard Things", contributor: "Ben Horowitz" }
   ]
 };
 
 const Songs = {
   rock: [
-    { name: "One  Moe Light", singer: "Linkin Park" },
-    { name: "Stars", singer: "Skillet" },
-    { name: "I Will Not Bow", singer: "Breaking Benjamin" }
+    { name: "One  Moe Light", contributor: "Linkin Park" },
+    { name: "Stars", contributor: "Skillet" },
+    { name: "I Will Not Bow", contributor: "Breaking Benjamin" }
   ],
 
   soul: [
-    { name: "Visiting Hours", singer: "Ed Sheeran" },
-    { name: "Dil Ka Dariya", singer: "Arijit Sing" },
-    { name: "Teri Mitti", singer: "B Praak" }
+    { name: "Visiting Hours", contributor: "Ed Sheeran" },
+    { name: "Dil Ka Dariya", contributor: "Arijit Sing" },
+    { name: "Teri Mitti", contributor: "B Praak" }
   ],
 
   rap: [
-    { name: "Lose Yourself", singer: "EMINEM" },
-    { name: "Aathma Raama", singer: "Brodha V" },
-    { name: "Lost in The Echo", singer: "Linkin Park" }
+    { name: "Lose Yourself", contributor: "EMINEM" },
+    { name: "Aathma Raama", contributor: "Brodha V" },
+    { name: "Lost in The Echo", contributor: "Linkin Park" }
   ]
 };
 
 const Movies = {
   action: [
-    { name: "Marvel Avengers Series", director: "Anthony & Joe Russo" },
-    { name: "Justice League", director: "Zack Snyder" },
-    { name: "R.R.R.", director: "S. S. Rajamouli" }
+    { name: "Marvel Avengers Series", contributor: "Anthony & Joe Russo" },
+    { name: "Justice League", contributor: "Zack Snyder" },
+    { name: "R.R.R.", contributor: "S. S. Rajamouli" }
   ],
 
   thriller: [
-    { name: "Rocketry: The Nambi Effect", director: "R Madhavan" },
-    { name: "Uri: The Surgical Strike", director: "Aditya Dhar" },
-    { name: "Shershaah", director: "Vishnuvardhan" }
+    { name: "Rocketry: The Nambi Effect", contributor: "R Madhavan" },
+    { name: "Uri: The Surgical Strike", contributor: "Aditya Dhar" },
+    { name: "Shershaah", contributor: "Vishnuvardhan" }
   ],
 
   sliceOfLife: [
-    { name: "777 Charlie", director: "Kiranraj K" },
-    { name: "Hachi: A Dog's Tale", director: "Lasse Hallström" },
-    { name: "Dead Poets Society", director: "Peter Weir" }
+    { name: "777 Charlie", contributor: "Kiranraj K" },
+    { name: "Hachi: A Dog's Tale", contributor: "Lasse Hallström" },
+    { name: "Dead Poets Society", contributor: "Peter Weir" }
   ]
 };
 
@@ -69,22 +69,48 @@ var dataBase = { Books, Songs, Movies };
 
 export default function App() {
   let [selectedType, setType] = useState("");
-  let [subType, setSubType] = useState("");
   let [dataBase2, setDataBase2] = useState("");
-
+  let [selectedType2, setType2] = useState("");
   function typeClickHandler(type) {
     setType(type);
-    if (selectedType !== "") {
-      console.log(Object.keys(dataBase[selectedType]));
+    // if (selectedType !== "") {
+    //   dataBase2 = Object.keys(dataBase[selectedType]);
+    //   console.log(dataBase2);
+    //   console.log(Object.keys(dataBase[selectedType]));
+    // }
+    if (type === "Books") {
+      setDataBase2(Books);
+      console.log(Books);
+    }
+    if (type === "Songs") {
+      setDataBase2(Songs);
+      console.log(Songs);
+    }
+    if (type === "Movies") {
+      setDataBase2(Movies);
+      console.log(Movies);
     }
   }
 
-  function subTypeClickHandler(type2) {
-    setSubType(type2);
-    if (selectedType !== "") {
-      setDataBase2(Object.keys(dataBase[selectedType]));
-      console.log(Object.keys(dataBase[selectedType]));
-    }
+  function genreClickHandler(type2) {
+    // setDataBase2(typeClickHandler(selectedType));
+    setType2(type2);
+    setType(selectedType2);
+    console.log(selectedType2);
+    console.log(dataBase2[selectedType2]);
+
+    // if (typeClickHandler(selectedType) === "Books") {
+    //   setDataBase2(Books);
+    //   console.log(Books);
+    // }
+    // if (typeClickHandler(selectedType) === "Songs") {
+    //   setDataBase2(Songs);
+    //   console.log(Songs);
+    // }
+    // if (typeClickHandler(selectedType) === "Movies") {
+    //   setDataBase2(Movies);
+    //   console.log(Movies);
+    // }
   }
 
   return (
@@ -112,7 +138,7 @@ export default function App() {
       <div>
         {Object.keys(dataBase2).map((type2) => (
           <button
-            onClick={() => subTypeClickHandler(type2)}
+            onClick={() => genreClickHandler(type2)}
             key={type2}
             style={{
               cursor: "pointer",
@@ -126,6 +152,28 @@ export default function App() {
             {type2}
           </button>
         ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {Object.keys(dataBase2[selectedType2]).map((option) => (
+            <li
+              key={option.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {option.name} </div>
+              <div style={{ fontSize: "smaller" }}> {option.rating} </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
