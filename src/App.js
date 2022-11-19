721ab29e4f214204e32c0d0cb4bd2372bf9e1a25
Perseus <54466064+Perseusx99007@users.noreply.github.com>
2022-11-19 11:19:58 +0530

    Documentation

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>💯 ⭐Awesone Recommendations📚</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Checkout my favorite books. Select a genre to get started</h2>
     </div>
   );
 }
